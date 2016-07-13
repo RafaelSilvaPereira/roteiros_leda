90af4dea37c68f6dd57663ef5de1022d9a52ef4c
Eric Breno <eric.santos@ccc.ufcg.edu.br>
2016-07-13 00:17:39 -0300

    adicionada melhoria, nao se usa mais o passo recursivo para problemas de tamanho menor que 3

--- Eric/rot-ord-02/src/main/java/sorting/divideAndConquer/quicksort3/QuickSortMedianOfThree.java
+++ Eric/rot-ord-02/src/main/java/sorting/divideAndConquer/quicksort3/QuickSortMedianOfThree.java
@@ -20,17 +20,35 @@ import sorting.Util;
  */
 public class QuickSortMedianOfThree<T extends Comparable<T>> extends AbstractSorting<T>{
     
-	public void sort(T[] array, int leftIndex, int rightIndex){
+	public void sort(T[] array, int leftIndex, int rightIndex) {
 		if (array.length > 1 && rightIndex - leftIndex >= 1) {
 			sortPivots(array, leftIndex, rightIndex);
 			int posPivot = moveElems(array, leftIndex + 1, rightIndex - 1);
-			sort(array, leftIndex, posPivot - 1);
-			sort(array, posPivot + 1, rightIndex);
+			if (rightIndex - leftIndex > 2) {
+				sort(array, leftIndex, posPivot - 1);
+				sort(array, posPivot + 1, rightIndex);
+			} else {
+				sortThree(array, leftIndex, rightIndex);
+			}
 		}
 	}
-	
+
+	private void sortThree(T[] array, int leftIndex, int rightIndex) {
+		int size = rightIndex - leftIndex;
+		if (size > 0 && array[leftIndex].compareTo(array[rightIndex]) > 0) {
+			Util.swap(array, leftIndex, rightIndex);
+		}
+		if (size > 1) {
+			int middle = (leftIndex + rightIndex) / 2;
+			if (array[leftIndex].compareTo(array[middle]) > 0)
+				Util.swap(array, leftIndex, middle);
+			if (array[middle].compareTo(array[rightIndex]) > 0)
+				Util.swap(array, middle, rightIndex);
+		}
+	}
+
 	private int moveElems(T[] array, int start, int end) {
-		int backPointer = start -1;
+		int backPointer = start - 1;
 		int frontPointer = start;
 		T pivot = array[end];
 
@@ -47,7 +65,7 @@ public class QuickSortMedianOfThree<T extends Comparable<T>> extends AbstractSor
 			Util.swap(array, end, end - 1);
 			end--;
 		}
-//		Util.swap(array, backPointer, end);
+		// Util.swap(array, backPointer, end);
 		return backPointer;
 	}
 
