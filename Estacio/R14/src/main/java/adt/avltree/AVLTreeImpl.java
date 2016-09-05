68ddaf7722e597237d9bd7fb16031a4b6ef276bb
Estacio Pereira <estacio.pereira@gmail.com>
2016-09-05 17:09:13 -0300

    AVL

---
+++ Estacio/R14/src/main/java/adt/avltree/AVLTreeImpl.java
@@ -0,0 +1,105 @@
+package adt.avltree;
+
+import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
+
+/**
+ * Performs consistency validations within a AVL Tree instance
+ *
+ * @param <T>
+ * @author Claudio Campelo
+ */
+public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements
+        AVLTree<T> {
+
+    public static final int ZERO = 0;
+    public static final int UNBALANCED = 2;
+    public static final int UNBALANCED_LEFT = 1;
+    public static final int UNBALANCED_RIGHT = -1;
+
+    // TODO Do not forget: you must override the methods insert and remove
+    // conveniently.
+
+    // AUXILIARY
+    protected int calculateBalance(BSTNode<T> node) {
+        return (node.isEmpty()) ? ZERO :
+                this.height((BSTNode<T>) node.getLeft()) - this.height((BSTNode<T>) node.getRight());
+    }
+
+    // AUXILIARY
+    protected void rebalance(BSTNode<T> node) {
+        int balance = this.calculateBalance(node);
+        if (balance > UNBALANCED_LEFT) {
+            BSTNode<T> leftChild = (BSTNode<T>) node.getLeft();
+            if (this.isOneDegree(leftChild) && this.hasOnlyRightChild(leftChild)) {
+                this.leftRotation(leftChild);
+            }
+            this.rightRotation(node);
+        } else if (balance < UNBALANCED_RIGHT) {
+            BSTNode<T> rightChild = (BSTNode<T>) node.getRight();
+            if (this.isOneDegree(rightChild) && this.hasOnlyLeftChild(rightChild)) {
+                this.rightRotation(rightChild);
+            }
+            this.leftRotation(node);
+        }
+    }
+
+    // AUXILIARY
+    protected void rebalanceUp(BSTNode<T> node) {
+        int balance = this.calculateBalance(node);
+        if (Math.abs(balance) >= UNBALANCED) {
+            this.rebalance(node);
+        }
+        if (node.getParent() != null) {
+            this.rebalanceUp((BSTNode<T>) node.getParent());
+        }
+    }
+
+    // AUXILIARY
+    protected void leftRotation(BSTNode<T> node) {
+        BSTNode<T> rightChild = (BSTNode<T>) node.getRight();
+
+        node.setRight(rightChild.getLeft());
+        rightChild.setLeft(node);
+        if (this.isLeftChild(node)) {
+            node.getParent().setLeft(rightChild);
+        } else if (node.getParent() != null) {
+            node.getParent().setRight(rightChild);
+        }
+        rightChild.setParent(node.getParent());
+        node.setParent(rightChild);
+    }
+
+    // AUXILIARY
+    protected void rightRotation(BSTNode<T> node) {
+        BSTNode<T> leftChild = (BSTNode<T>) node.getLeft();
+
+        node.setLeft(leftChild.getRight());
+        leftChild.setRight(node);
+        if (node.getParent() == null) {
+            this.root = leftChild;
+        } else if (this.isLeftChild(node)) {
+            node.getParent().setLeft(leftChild);
+        } else {
+            node.getParent().setRight(leftChild);
+        }
+        leftChild.setParent(node.getParent());
+        node.setParent(leftChild);
+    }
+
+    @Override
+    public void insert(T element) {
+        super.insert(element);
+        BSTNode<T> node = this.search(element);
+        this.rebalanceUp(node);
+    }
+
+    @Override
+    public void remove(T element) {
+        BSTNode<T> node = this.search(element);
+        if (!node.isEmpty()) {
+            super.remove(element);
+            this.rebalanceUp(node);
+        }
+    }
+}
