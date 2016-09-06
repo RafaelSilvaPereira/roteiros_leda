b16cae081e186a361d882cfecffdbbde99d55578
Estacio Pereira <estacio.pereira@gmail.com>
2016-09-06 09:57:02 -0300

    super hot-fix remove

--- Estacio/R14/src/test/java/adt/avltree/StudentAVLTest2.java
+++ Estacio/R14/src/test/java/adt/avltree/StudentAVLTest2.java
@@ -68,6 +68,29 @@ public class StudentAVLTest2 {
 		avl.remove(55);
 		assertEquals(NIL, avl.getRoot());
 		assertTrue(avl.isEmpty());
+
+		avl = new AVLTreeImpl<>();
+		avl.insert(10);
+		avl.insert(15);
+		avl.insert(6);
+		avl.insert(4);
+		avl.insert(8);
+		avl.insert(12);
+		avl.insert(18);
+		avl.insert(2);
+		avl.insert(5);
+		avl.insert(7);
+		avl.insert(9);
+		avl.insert(13);
+		avl.insert(16);
+		avl.insert(19);
+		avl.insert(3);
+		avl.insert(17);
+
+		//((BSTNode)avl.getRoot()).printTree();
+		avl.remove(10);
+		//((BSTNode)avl.getRoot()).printTree();
+		assertEquals(avl.getRoot().getRight().getData(), new Integer(16));
 	}
 	
 	@Test
